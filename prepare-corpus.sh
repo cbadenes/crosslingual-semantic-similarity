fe71f7cd11af0ab84c8aeb7817d188df83105463
Carlos Badenes <cbadenes@gmail.com>
2018-09-13 17:14:52 +0200

    fixed name

--- prepare-corpus.sh
+++ prepare-corpus.sh
@@ -2,4 +2,4 @@
 mvn clean compile exec:java -Dexec.mainClass="io.github.cbadenes.crosslingual.tasks.CorpusDownload"
 mvn clean compile exec:java -Dexec.mainClass="io.github.cbadenes.crosslingual.tasks.CorpusPrepare"
 mvn clean compile exec:java -Dexec.mainClass="io.github.cbadenes.crosslingual.tasks.CorpusPrepareStatistics"
-mvn clean compile exec:java -Dexec.mainClass="io.github.cbadenes.crosslingual.tasks.CorpusSplitAndTest"
+mvn clean compile exec:java -Dexec.mainClass="io.github.cbadenes.crosslingual.tasks.CorpusSplitTrainAndTest"
